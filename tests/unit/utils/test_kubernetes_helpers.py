ee7b7474d9c31e883f0cd5f6c609a845c52ee47f
Steven Robertson <steven@nervanasys.com>
2018-02-28 15:58:40 -0800

    fixed kubernetes_helpers_tests

--- tests/unit/utils/test_kubernetes_helpers.py
+++ tests/unit/utils/test_kubernetes_helpers.py
@@ -7,19 +7,21 @@ from mlt.utils.kubernetes_helpers import ensure_namespace_exists
 
 @patch('mlt.utils.kubernetes_helpers.call')
 @patch('mlt.utils.kubernetes_helpers.open')
-@patch('mlt.commands.init.process_helpers')
-def test_ensure_namespace_no_exist(call_mock, open_mocker, proc_helpers_mock):
-    call_mock.return_value = 0
+@patch('mlt.utils.kubernetes_helpers.process_helpers')
+def test_ensure_namespace_no_exist(proc_helpers, open_mock, call):
+    call.return_value = 0
+    open_mock = MockOpen()
+
     ensure_namespace_exists(str(uuid.uuid4()))
-    assert proc_helpers_mock.assert_not_called()
+    proc_helpers.run.assert_not_called()
 
 
 @patch('mlt.utils.kubernetes_helpers.call')
 @patch('mlt.utils.kubernetes_helpers.open')
-@patch('mlt.commands.init.process_helpers')
-def test_ensure_namespace_already_exists(call_mock, open_mocker,
-                                         proc_helpers_mock):
-    new_namespace = str(uuid.uuid4())
-    ensure_namespace_exists(new_namespace)
-    ensure_namespace_exists(new_namespace)
-    assert proc_helpers_mock.assert_called_once()
+@patch('mlt.utils.kubernetes_helpers.process_helpers')
+def test_ensure_namespace_already_exists(proc_helpers, open_mock, call):
+    open_mock = MockOpen()
+    call.return_value = 1
+
+    ensure_namespace_exists(str(uuid.uuid4()))
+    proc_helpers.run.assert_called_once()
