a47159f60c5e9a47417ccf43a1f663bc3ad395fe
Steven Robertson <steven@nervanasys.com>
2018-03-02 17:41:13 -0800

    added more things to test in e2e tests; difficulty in testing locally though possibly due to proxy

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:16.04
 
-RUN apt-get update
+# fix-missing is needed to get `git` to work
+RUN apt-get update --fix-missing
+RUN apt-get upgrade -y
 RUN apt-get install -y apt-transport-https \
     ca-certificates \
     curl \
+    git \
     software-properties-common
 RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 RUN add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
--- tests/e2e/test_deploy_flow.py
+++ tests/e2e/test_deploy_flow.py
@@ -5,6 +5,8 @@ import subprocess
 import tempfile
 import os
 
+from mlt.utils.process_helpers import run, run_popen_unsecure
+
 
 @contextmanager
 def create_work_dir():
@@ -12,7 +14,7 @@ def create_work_dir():
     try:
         yield workdir
     finally:
-        # still need to remove dir when done
+        # even on error we still need to remove dir when done
         # https://docs.python.org/2/library/tempfile.html#tempfile.mkdtemp
         shutil.rmtree(workdir)
 
@@ -39,6 +41,10 @@ def test_flow():
                 'name': 'foobar',
                 'registry': 'localhost:5000'
             }
+        # verify we created a git repo with our project init
+        assert "On branch master" in run(
+            "git --git-dir={}/.git --work-tree={} status".format(
+                project_dir, project_dir).split())
 
         # mlt build
         p = subprocess.Popen(['mlt', 'build'], cwd=project_dir)
@@ -48,6 +54,10 @@ def test_flow():
             build_data = json.loads(f.read())
             assert 'last_container' in build_data and \
                 'last_build_duration' in build_data
+        # verify that we created a docker image with repo name `foobar`
+        assert run_popen_unsecure(
+            "docker images | awk '{print $1}' | sed -n 2p"
+        ).stdout.read().strip() == 'foobar'
 
         # mlt deploy
         p = subprocess.Popen(['mlt', 'deploy'], cwd=project_dir)
@@ -58,7 +68,20 @@ def test_flow():
             deploy_data = json.loads(f.read())
             assert 'last_push_duration' in deploy_data and \
                 'last_remote_container' in deploy_data
+        # verify that the docker image has been pushed to our local registry
+        assert run_popen_unsecure(
+            "curl --noproxy \"*\"  localhost:5000/v2/_catalog | "
+            "jq .repositories | jq 'contains([\"foobar\"])'"
+        ).stdout.read().strip() == 'true'
+        # verify that our job did indeed get deployed to k8s
+        assert run_popen_unsecure(
+            "kubectl get jobs --namespace=foobar | awk '{print $1}' | "
+            "sed -n 2p").stdout.read().strip() is not None
 
         # mlt undeploy
         p = subprocess.Popen(['mlt', 'undeploy'], cwd=project_dir)
         assert p.wait() == 0
+        # verify no more deployment job
+        assert run_popen_unsecure(
+            "kubectl get jobs --namespace=foobar | awk '{print $1}' | "
+            "sed -n 2p").stdout.read().strip() == "No resources found."
