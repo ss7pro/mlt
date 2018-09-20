6cc0486aaafd41672049ae288719080589236c03
Karthik Vadla <karthik.vadla@intel.com>
2018-09-20 16:18:09 -0700

    Add unet single node

---
+++ examples/unet_single_node/mlt_app/deploy.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+# install ksonnet + kubeflow openmpi so we can support Horovod distributed training
+
+VERSION=master
+SECRET=openmpi-secret
+# https://github.com/ksonnet/ksonnet/issues/298
+export USER=root
+
+# set your GITHUB_TOKEN otherwise deployment will fail with API Limit error
+export GITHUB_TOKEN=$GITHUB_TOKEN
+
+# Please update physical cores value. Below command works for linux.
+#export PHYSICAL_CORES=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` # Total number of physical cores per socket
+export PHYSICAL_CORES=4
+
+{
+# Generate one-time ssh keys used by Open MPI.
+SECRET=openmpi-secret
+mkdir -p .tmp
+yes 2>/dev/null | ssh-keygen -N "" -f .tmp/id_rsa
+kubectl delete secret ${SECRET} -n ${NAMESPACE} || true
+kubectl create secret generic ${SECRET} -n ${NAMESPACE} --from-file=id_rsa=.tmp/id_rsa --from-file=id_rsa.pub=.tmp/id_rsa.pub --from-file=authorized_keys=.tmp/id_rsa.pub
+rm -rf .tmp
+
+# Initialize a ksonnet app. Set the namespace for it's default environment.
+if [ -d "$JOB_NAME" ]; then
+  pushd .
+  cd ${JOB_NAME}
+  set +e
+  ks delete default
+  ks component rm ${JOB_NAME}
+  set -e
+  popd
+  rm -rf ${JOB_NAME}
+fi
+
+ks init ${JOB_NAME}
+cd ${JOB_NAME}
+ks env set default --namespace ${NAMESPACE}
+
+# Install openmpi kubeflow component.
+ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${VERSION}/kubeflow
+ks pkg install kubeflow/openmpi@${VERSION}
+
+# Temporary fix for volume-mount
+# Untill this is merged : https://github.com/kubeflow/kubeflow/issues/838
+cp -rf ../volume-mount/workloads.libsonnet vendor/kubeflow/openmpi/
+cp -rf ../volume-mount/prototypes/openmpi.jsonnet vendor/kubeflow/openmpi/prototypes/
+
+# Generate openmpi components.
+
+# Node selector helps to launch job on specific set of nodes with label.
+# For kubernetes nodes we assigned label called `node-type=highmem` to set of nodes.
+# Ex: kubectl label node gke-node-1 node-type=highmem
+NODE_SELECTOR="node-type=highmem"
+
+COMPONENT=${JOB_NAME}
+
+# data path for training data
+DATA_PATH=${DATA_PATH}
+
+# output path to store results
+OUTPUT_PATH=${OUTPUT_PATH}
+
+IMAGE=${IMAGE}
+WORKERS=$(( ${NUM_NODES} * ${NUM_WORKERS_PER_NODE} ))
+SOCKETS_PER_NODE=${SOCKETS_PER_NODE}
+NUM_INTER_THREADS=${NUM_INTER_THREADS}
+
+PPR=$(( $NUM_WORKERS_PER_NODE / $SOCKETS_PER_NODE ))
+
+GPU=${GPUS}
+EXEC="mpirun -np ${WORKERS} \
+--hostfile /kubeflow/openmpi/assets/hostfile \
+--map-by socket \
+-cpus-per-proc $PHYSICAL_CORES \
+--report-bindings \
+--oversubscribe bash /src/app/exec_multiworker.sh ${PPR} ${NUM_INTER_THREADS} ${DATA_PATH} ${OUTPUT_PATH}"
+
+ks generate openmpi ${COMPONENT} --image ${IMAGE} --secret ${SECRET} --workers ${WORKERS} --gpu ${GPU} --exec "${EXEC}" --nodeSelector "${NODE_SELECTOR}"
+} &> /dev/null
+
+# Uncomment below params to mount data.
+
+# If you have data on your host, if you want to mount that as volume. Please update below paths
+# volumes - path in this section will create a volume for you based on host path provided
+# volumeMounts - mountPath in this section will mount above volume at specified location
+if [ -n "${DATA_PATH}" ];
+    then
+        ks param set ${COMPONENT} volumes '[{ "name": "vol", "hostPath": { "path": "'"${DATA_PATH}"'"}}]'
+        ks param set ${COMPONENT} volumeMounts '[{ "name": "vol", "mountPath": "'"${DATA_PATH}"'"}]'
+fi
+
+# Deploy to your cluster.
+ks apply default
---
+++ examples/unet_single_node/mlt_app/main.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+"""
+Runs U-Net training on BraTS dataset
+"""
+import tensorflow as tf
+from tensorflow import layers
+
+from model import define_model, validate_model
+from data import load_datasets, get_batch
+
+import os
+import psutil
+from datetime import datetime
+
+import settings
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer("num_inter_threads", 2,
+                            "# inter op threads")
+
+tf.app.flags.DEFINE_integer("num_threads", psutil.cpu_count(logical=False),
+                            "# intra op threads")
+
+tf.app.flags.DEFINE_integer("epochs", settings.EPOCHS,
+                            "Number of epochs to train")
+
+tf.app.flags.DEFINE_integer("log_steps", 5,
+                            "Number of steps between logs")
+
+tf.app.flags.DEFINE_integer("batch_size", settings.BATCH_SIZE,
+                            "Batch Size for Training")
+
+tf.app.flags.DEFINE_string("output_path", settings.OUTPUT_PATH,
+                            "Output log directory")
+
+tf.app.flags.DEFINE_string("data_path",
+                            settings.DATA_PATH,
+                            "Data directory")
+
+tf.app.flags.DEFINE_boolean("no_horovod", True,
+                            "Don't use Horovod. Single node training only.")
+tf.app.flags.DEFINE_float("learningrate", settings.LEARNING_RATE,
+                            "Learning rate")
+tf.app.flags.DEFINE_boolean("use_upsampling", settings.USE_UPSAMPLING,
+                        "True = Use upsampling; False = Use transposed convolution")
+
+os.environ["KMP_BLOCKTIME"] = "0"
+os.environ["KMP_AFFINITY"] = "granularity=thread,compact,1,0"
+os.environ["OMP_NUM_THREADS"] = str(FLAGS.num_threads)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Get rid of the AVX, SSE warnings
+
+config = tf.ConfigProto(intra_op_parallelism_threads=FLAGS.num_threads,
+                        inter_op_parallelism_threads=FLAGS.num_inter_threads)
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+if not FLAGS.no_horovod:
+    import horovod.tensorflow as hvd
+
+
+def main(_):
+
+    start_time = datetime.now()
+    tf.logging.info("Data path: {}".format(FLAGS.data_path))
+    tf.logging.info("Output path: {}".format(FLAGS.output_path))
+    tf.logging.info("Starting at: {}".format(start_time))
+    tf.logging.info("Batch size: {} images per step".format(FLAGS.batch_size))
+
+    last_epoch_start_time = start_time
+
+    # Load datasets
+    imgs_train, msks_train, imgs_test, msks_test = load_datasets(FLAGS)
+
+    if not FLAGS.no_horovod:
+        # Initialize Horovod.
+        hvd.init()
+
+    # Define model
+    model = define_model(imgs_train.shape, msks_train.shape, FLAGS)
+
+    if not FLAGS.no_horovod:
+        # Horovod: adjust learning rate based on number workers
+        opt = tf.train.AdamOptimizer(learning_rate=FLAGS.learningrate,
+                                      epsilon=tf.keras.backend.epsilon())
+        #opt = tf.train.RMSPropOptimizer(0.0001 * hvd.size())
+        # tf.logging.info("HOROVOD: New learning rate is {}".\
+        #         format(FLAGS.learningrate * hvd.size()))
+    else:
+        opt = tf.train.AdamOptimizer(learning_rate=FLAGS.learningrate,
+                                      epsilon=tf.keras.backend.epsilon())
+        #opt = tf.train.RMSPropOptimizer(0.0001)
+
+    # Wrap optimizer with Horovod Distributed Optimizer.
+    if not FLAGS.no_horovod:
+        tf.logging.info("HOROVOD: Wrapped optimizer")
+        opt = hvd.DistributedOptimizer(opt)
+
+    global_step = tf.train.get_or_create_global_step()
+    train_op = opt.minimize(model["loss"], global_step=global_step)
+
+    train_length = len(imgs_train)
+    total_steps = (FLAGS.epochs * train_length) // FLAGS.batch_size
+    if not FLAGS.no_horovod:
+        last_step = total_steps // hvd.size()
+        validation_steps = train_length // FLAGS.batch_size // hvd.size()
+    else:
+        last_step = total_steps
+        validation_steps = train_length // FLAGS.batch_size
+
+    def formatter_log(tensors):
+        """
+        Format the log output
+        """
+        if FLAGS.no_horovod:
+            logstring = "Step {} of {}: " \
+               " training Dice loss = {:.4f}," \
+               " training Dice = {:.4f}".format(tensors["step"],
+               last_step,
+               tensors["loss"], tensors["dice"])
+        else:
+            logstring = "HOROVOD (Worker #{}), Step {} of {}: " \
+               " training Dice loss = {:.4f}," \
+               " training Dice = {:.4f}".format(
+               hvd.rank(),
+               tensors["step"],
+               last_step,
+               tensors["loss"], tensors["dice"])
+
+        return logstring
+
+    hooks = [
+
+        tf.train.StopAtStepHook(last_step=last_step),
+
+        # Prints the loss and step every log_steps steps
+        tf.train.LoggingTensorHook(tensors={"step": global_step,
+                                    "loss": model["loss"],
+                                    "dice": model["metric_dice"]},
+                                   every_n_iter=FLAGS.log_steps,
+                                   formatter=formatter_log),
+    ]
+
+    # Horovod: BroadcastGlobalVariablesHook broadcasts
+    # initial variable states from rank 0 to all other
+    # processes. This is necessary to ensure consistent
+    # initialization of all workers when training is
+    # started with random weights
+    # or restored from a checkpoint.
+    if not FLAGS.no_horovod:
+        hooks.append(hvd.BroadcastGlobalVariablesHook(0))
+
+        # Horovod: save checkpoints only on worker 0 to prevent other workers from
+        # corrupting them.
+        if hvd.rank() == 0:
+            checkpoint_dir = "{}/{}-workers/{}".format(FLAGS.output_path,
+                            hvd.size(),
+                            datetime.now().strftime("%Y%m%d-%H%M%S"))
+            print(checkpoint_dir)
+        else:
+            checkpoint_dir = None
+
+    else:
+        checkpoint_dir = "{}/no_hvd/{}".format(FLAGS.output_path,
+                        datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # The MonitoredTrainingSession takes care of session initialization,
+    # restoring from a checkpoint, saving to a checkpoint,
+    # and closing when done or an error occurs.
+    current_step = 0
+    startidx = 0
+    epoch_idx = 0
+
+    with tf.train.MonitoredTrainingSession(checkpoint_dir=checkpoint_dir,
+                                           hooks=hooks,
+                                           save_summaries_steps=FLAGS.log_steps,
+                                           log_step_count_steps=FLAGS.log_steps,
+                                           config=config) as mon_sess:
+
+        while not mon_sess.should_stop():
+
+            # Run a training step synchronously.
+            image_, mask_ = get_batch(imgs_train,
+                                      msks_train,
+                                      FLAGS.batch_size)
+
+            # Do batch in order
+            # stopidx = startidx + FLAGS.batch_size
+            # if (stopidx > train_length):
+            #     stopidx = train_length
+            #
+            # image_ = imgs_train[startidx:stopidx]
+            # mask_  = msks_train[startidx:stopidx]
+
+            mon_sess.run(train_op, feed_dict={model["input"]: image_,
+                                              model["label"]: mask_})
+
+            current_step += 1
+            # # Get next batch (loop around if at end)
+            # startidx += FLAGS.batch_size
+            # if (startidx > train_length):
+            #     startidx = 0
+
+    stop_time = datetime.now()
+    tf.logging.info("Stopping at: {}".format(stop_time))
+    tf.logging.info("Elapsed time was: {}".format(stop_time-start_time))
+
+if __name__ == "__main__":
+
+    os.system("hostname")
+    os.system("lscpu")
+    tf.logging.info("{}".format(FLAGS.flag_values_dict()))
+    tf.app.run()
