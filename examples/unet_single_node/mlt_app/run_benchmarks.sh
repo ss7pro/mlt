a5795bf038725b1ece6817f6263ea8df8de5aa97
Karthik Vadla <karthik.vadla@intel.com>
2018-09-20 17:42:57 -0700

    add conda env for bare metal

--- examples/unet_single_node/mlt_app/run_benchmarks.sh
+++ examples/unet_single_node/mlt_app/run_benchmarks.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+conda_env=${3}
+
 inter=2
 
 physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
 num_sockets=`lscpu | grep "Socket(s)" | cut -d':' -f2 | sed "s/ //g"`
 num_cores=$(( $physical_cores * $num_sockets ))
 
+if [ -n "${conda_env}" ]
+ then
+     echo -e "\nActivating ${conda_env}\n"
+     source ~/.bashrc
+     conda activate ${conda_env}
+fi
+
 python main.py --num_inter_threads=${inter} --num_threads=$num_cores --no_horovod --data_path=${1} --output_path=${2}
+
+if [ -n "${conda_env}" ]
+ then
+     echo -e "\nDeactivating ${conda_env}\n"
+     conda deactivate
+fi
